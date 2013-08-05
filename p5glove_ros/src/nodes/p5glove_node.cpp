06bc144ed5a08dff5f1082ad893e5d1befbab0b1
System User <sampfeiffer@slovakia.unassigned-domain>
2013-08-05 20:18:40 +0200

    Now we have a pose displayed in rviz of how the hand is moved with the glove. And the Axis are corrected

--- p5glove_ros/src/nodes/p5glove_node.cpp
+++ p5glove_ros/src/nodes/p5glove_node.cpp
@@ -1,9 +1,16 @@
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <p5glove_ros/GloveData.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <std_msgs/Header.h>
 
 #include <p5glove_ros/p5glove.h>
 #include <errno.h>
+//#include <tf/tf.h>
+//#include <tf/LinearMath.h>
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include <math.h>
 
 int main(int argc, char **argv)
 {
@@ -17,6 +24,9 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "p5glove_node");
   ros::NodeHandle nh;
   ros::Publisher pub = nh.advertise<p5glove_ros::GloveData>("glove_data", 100);
+  ros::Publisher pub_pose = nh.advertise<geometry_msgs::PoseStamped>("glove_posestamped", 100);
+  ros::Publisher pub_pose2 = nh.advertise<geometry_msgs::PoseStamped>("glove_posestamped2", 100);
+  ros::Publisher pub_pose3 = nh.advertise<geometry_msgs::PoseStamped>("glove_posestamped3", 100);
 
 
 
@@ -53,27 +63,42 @@ int main(int argc, char **argv)
     }
 
     p5glove_get_position(glove, pos);
-    msg.pose.position.x = pos[0];
-    msg.pose.position.y = pos[1];
-    msg.pose.position.z = pos[2];
+//    msg.pose.position.x = pos[0];
+//    msg.pose.position.y = pos[1];
+//    msg.pose.position.z = pos[2];
+
+    /* they use weird axis */
+    msg.pose.position.x = - pos[2]; // X on gloves axis (see PDF)
+    msg.pose.position.y = - pos[0]; // Y
+    msg.pose.position.z = pos[1];   // Z
     ROS_DEBUG_STREAM("Position:("
                      << pos[0] << ","
                      << pos[1] << ","
                      << pos[2] << ")");
 
     p5glove_get_rotation(glove, &angle, axis);
-    msg.pose.orientation.x = axis[0];
-    msg.pose.orientation.y = axis[1];
-    msg.pose.orientation.z = axis[2];
-    msg.pose.orientation.w = angle;
+    // from axis-angle representation to Quaternion, but i think this axis is wrong as the position
+    tf::Quaternion quat (tf::Vector3(- axis[2], - axis[0], axis[1]), angle * M_PI / 180.0);
+    msg.pose.orientation.x = quat.getX();
+    msg.pose.orientation.y = quat.getY();
+    msg.pose.orientation.z = quat.getZ();
+    msg.pose.orientation.w = quat.getW();
+
     ROS_DEBUG_STREAM("Orientation:("
-                     << axis[0] << ","
-                     << axis[1] << ","
-                     << axis[2] << ","
-                     << angle << ")");
+                     << msg.pose.orientation.x << ","
+                     << msg.pose.orientation.y << ","
+                     << msg.pose.orientation.z << ","
+                     << msg.pose.orientation.w << ")");
 
     msg.header.stamp = ros::Time::now();
     pub.publish(msg);
+    geometry_msgs::PoseStamped posestamped;
+    posestamped.pose = msg.pose;
+    posestamped.header.stamp= ros::Time::now();
+    posestamped.header.frame_id = "base_link";
+    pub_pose.publish(posestamped);
+
+
     ros::spinOnce();
   }
   p5glove_close(glove);
